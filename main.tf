de9a002c34423af0f338b73bfb9801b651fbce1e
TeknoPathshala <95009959+TeknoPathshala@users.noreply.github.com>
2022-01-11 13:34:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,13 +45,12 @@ resource "aws_instance" "myFirstInstance" {
   tags= {
     Name = var.tag_name
   }
- user_data = <<EOF
+user_data = <<EOF
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo yum update -y
 sudo amazon-linux-extras install nginx1 -y
-sudo su -c "/bin/echo 'My Site: DevopsRoles.com' >/usr/share/nginx/html/index.html"
-instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+sudo su -c "/bin/echo 'My Site: teknopathshala' >/usr/share/nginx/html/index.html"
 sudo su -c "echo $instance_ip >>/usr/share/nginx/html/index.html"
 sudo systemctl start nginx
 sudo systemctl enable  nginx
